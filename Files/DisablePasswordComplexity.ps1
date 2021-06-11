be5607cb6ed157b1b63239207bd8d7d571b48373
nysmm <nysm.private@gmail.com>
2021-06-12 01:04:58 +1000

    Push

--- Files/DisablePasswordComplexity.ps1
+++ Files/DisablePasswordComplexity.ps1
@@ -1,4 +1,1 @@
-secedit /export /cfg c:\secpol.cfg
-(gc C:\secpol.cfg).replace("PasswordComplexity = 1", "PasswordComplexity = 0") | Out-File C:\secpol.cfg
-secedit /configure /db c:\windows\security\local.sdb /cfg c:\secpol.cfg /areas SECURITYPOLICY
-rm -force c:\secpol.cfg -confirm:$false
+404: Not Found
